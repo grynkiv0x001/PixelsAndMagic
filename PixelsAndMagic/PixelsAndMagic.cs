a20f85a71f42f8878b1eb79bfec8a71832fec448
Bohdan Hrynkiv <grynkiv0x001@gmail.com>
2026-01-08 08:22:02 +0100

    Project setup

---
+++ PixelsAndMagic/PixelsAndMagic.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace PixelsAndMagic;
+
+public class PixelsAndMagic : GameLibrary.Core
+{
+    private const int WINDOW_WIDTH = 1280;
+    private const int WINDOW_HEIGHT = 720;
+    private const bool FULLSCREEN = false;
+
+    public PixelsAndMagic() : base("PixelsAndMagic", WINDOW_WIDTH, WINDOW_HEIGHT, FULLSCREEN)
+    {
+        
+    }
+    
+    protected override void Draw(GameTime gameTime)
+    {
+        base.Draw(gameTime);
+    }
+}
