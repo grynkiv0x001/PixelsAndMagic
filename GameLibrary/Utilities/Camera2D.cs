94749dd1d83215378bd6bb727b7767c65d2f019c
Bohdan Hrynkiv <grynkiv0x001@gmail.com>
2026-01-16 09:21:14 +0100

    Added world; code splitting; improved camera

--- GameLibrary/Utilities/Camera2D.cs
+++ GameLibrary/Utilities/Camera2D.cs
@@ -1,16 +1,58 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace GameLibrary.Utilities;
 
 public class Camera2D
 {
+    private Vector2 _velocity;
+
     public Vector2 Position { get; set; }
+
     public float Zoom { get; set; } = 1.0f;
+
+    public int BoundaryPadding { get; set; } = 0;
+
+    public Rectangle WorldBounds { get; set; }
 
     public Matrix GetViewMatrix()
     {
         return
             Matrix.CreateTranslation(-Position.X, -Position.Y, 0) *
             Matrix.CreateScale(Zoom, Zoom, 1.0f);
+    }
+
+    public void Follow(
+        Vector2 target,
+        Viewport viewport,
+        float smoothing = 0.12f
+    )
+    {
+        // Desired camera position (center target)
+        var targetPosition = target - new Vector2(
+            viewport.Width * 0.5f / Zoom,
+            viewport.Height * 0.5f / Zoom
+        );
+
+        // Smoothly move towards target
+        Position = Vector2.Lerp(Position, targetPosition, smoothing);
+
+        ClampToWorld(viewport);
+    }
+
+    private void ClampToWorld(Viewport viewport)
+    {
+        var left = WorldBounds.Left - BoundaryPadding;
+        var top = WorldBounds.Top - BoundaryPadding;
+        var right = WorldBounds.Right + BoundaryPadding;
+        var bottom = WorldBounds.Bottom + BoundaryPadding;
+
+        var maxX = right - viewport.Width / Zoom;
+        var maxY = bottom - viewport.Height / Zoom;
+
+        Position = new Vector2(
+            MathHelper.Clamp(Position.X, left, maxX),
+            MathHelper.Clamp(Position.Y, top, maxY)
+        );
     }
 }
--- PixelsAndMagic/Scenes/GameScene.cs
+++ PixelsAndMagic/Scenes/GameScene.cs
@@ -13,28 +13,18 @@ public class GameScene : Scene
 {
     private Camera2D _camera;
 
-    private Enemy _enemy;
-
-    private SpriteFont _fontPixel;
-    private SpriteFont _fontRegular;
-
-    private Vector2 _gameInfoTextOrigin;
-    private Vector2 _gameInfoTextPosition;
-
-    private Player _player;
-
-    private Tilemap _world;
+    private World.World _world;
 
     public override void Initialize()
     {
         base.Initialize();
 
-        _camera = new Camera2D();
-
-        var infoTextYOrigin = _fontPixel.MeasureString("Health").Y * 0.5f;
-
-        _gameInfoTextPosition = new Vector2(32, 32);
-        _gameInfoTextOrigin = new Vector2(0, infoTextYOrigin);
+        // BoundaryPadding is the wall thickness 
+        _camera = new Camera2D
+        {
+            WorldBounds = _world.Bounds,
+            BoundaryPadding = 32 * 4
+        };
     }
 
     public override void LoadContent()
@@ -45,7 +35,7 @@ public class GameScene : Scene
         var enemySprite = enemySheet.CreateAnimatedSprite("enemy-animation");
         enemySprite.Scale = new Vector2(4.0f, 4.0f);
 
-        _enemy = new Enemy(enemySprite, new Vector2(600, 380));
+        var enemy = new Enemy(enemySprite, new Vector2(600, 380));
 
         // Player (Wizard) sprite loading
         var playerSheet = SpriteSheet.FromFile(Content, "Images/player-spritesheet.xml");
@@ -53,15 +43,13 @@ public class GameScene : Scene
         var playerSprite = playerSheet.CreateAnimatedSprite("wizard-animation");
         playerSprite.Scale = new Vector2(4.0f, 4.0f);
 
-        _player = new Player(playerSprite, Core.InputManager, new Vector2(200, 200), 100.0f, 20.0f);
+        var player = new Player(playerSprite, Core.InputManager, new Vector2(200, 200), 100.0f, 20.0f);
 
         // World loading (using the Tilemap)
-        _world = Tilemap.FromFile(Content, "Images/world-tilemap.xml");
-        _world.Scale = new Vector2(4.0f, 4.0f);
+        var world = Tilemap.FromFile(Content, "Images/world-tilemap.xml");
+        world.Scale = new Vector2(4.0f, 4.0f);
 
-        // Loading font
-        _fontPixel = Content.Load<SpriteFont>("Fonts/Jacquard_12");
-        _fontRegular = Content.Load<SpriteFont>("Fonts/Caudex");
+        _world = new World.World(world, player, [enemy]);
     }
 
     public override void Update(GameTime gameTime)
@@ -69,28 +57,13 @@ public class GameScene : Scene
         if (Core.InputManager.Keyboard.IsKeyPressed(Keys.Escape))
             Core.PushScene(new PauseScene());
 
-        // Screen boundaries
-        // TODO: Create a proper world instance
-        var tileWidth = (int)_world.TileWidth;
-        var tileHeight = (int)_world.TileHeight;
-
-        var screenBounds = new Rectangle(
-            tileWidth,
-            tileHeight,
-            (int)(_world.Columns * _world.TileWidth) - tileWidth * 2,
-            (int)(_world.Rows * _world.TileHeight) - tileHeight * 2
-        );
-
-        _player.Update(gameTime, screenBounds);
-        _enemy.Update(gameTime, screenBounds);
+        _world.Update(gameTime);
 
         // Camera follow
-        _camera.Position = _player.Position - new Vector2(
-            Core.Graphics.GraphicsDevice.Viewport.Width / 2,
-            Core.Graphics.GraphicsDevice.Viewport.Height / 2
+        _camera.Follow(
+            _world.Player.Position,
+            Core.Graphics.GraphicsDevice.Viewport
         );
-
-        if (_player.Collider.Intersects(_enemy.Collider)) _player.HandleEntityCollision(_enemy.Collider);
 
         base.Update(gameTime);
     }
@@ -103,21 +76,6 @@ public class GameScene : Scene
         );
 
         _world.Draw(SpriteBatch);
-
-        _player.Draw(SpriteBatch);
-        _enemy.Draw(SpriteBatch);
-
-        SpriteBatch.DrawString(
-            _fontPixel,
-            $"Health: {_player.Health}",
-            _gameInfoTextPosition,
-            Color.FloralWhite,
-            0.0f,
-            _gameInfoTextOrigin,
-            2.0f,
-            SpriteEffects.None,
-            0.0f
-        );
 
         SpriteBatch.End();
 
